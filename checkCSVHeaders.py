a427234c8ae170d409763ec26cc2de01755fc6d8
Ian Engelbrecht <ianicus.za@gmail.com>
2024-04-13 21:50:40 +0200

    added checkCSVHeaders and mergeCSVs

---
+++ checkCSVHeaders.py
@@ -0,0 +1,55 @@
+# Checks all csv files in a directory have a specific set of headers (case sensitive) and reports any that dont
+import os
+import csv
+
+dir = r'D:\NSCF Data WG\Current projects\Herbarum mass imaging\Data\Moss'
+headers = [
+  'barcode', 
+  'note', 
+  'dwc:family', 
+  'dwc:qualifier', 
+  'dwc:scientificName', 
+  'source', 
+  'identifier', 
+  'dwc:taxonomicStatus', 
+  'acceptedName'
+]
+
+### SCRIPT ###
+files = os.listdir(dir)
+csvs = list(filter(lambda x: x.lower().endswith('.csv'), files))
+
+errorCount = 0
+for csvFile in csvs:
+  with open(os.path.join(dir, csvFile), 'r', encoding="UTF8", errors="ignore") as f:
+    reader = csv.DictReader(f)
+    first = next(reader)
+
+    
+    csvHeaders = list(first.keys())
+    missing = list(set(headers) - set(csvHeaders))
+    missing = list(filter(None, missing))
+    additional = list(set(csvHeaders) - set(headers))
+    additional = list(filter(None, additional))
+
+    if len(missing) > 0:
+      print(csvFile, 'missing:', ', '.join(missing))
+      errorCount += 1
+    if len(additional) > 0:
+      print(csvFile, 'additional:', ', '.join(additional))
+      errorCount += 1
+
+    if '' in csvHeaders:
+      print(csvFile, 'has blank columns')
+      errorCount += 1
+      csvHeaders = list(filter(None, csvHeaders))
+
+    if len(missing) == 0 and len(additional) == 0:
+      if headers != csvHeaders:
+        print(csvFile, 'headers out of order')
+        errorCount += 1
+
+if errorCount == 0:
+  print('All headers are correct, have a nice day...')
+
+
---
+++ mergeCSVs.py
@@ -0,0 +1,58 @@
+# merges a set of CSVs from a directory into one file, and includes the file names in the dataset. 
+# note that all the CSVs have to have the same headers. Use checkCSVHeaders.py for that. 
+# This is an alternative xsv (https://github.com/BurntSushi/xsv) where you want to include file names. 
+
+import os
+import csv
+
+dir = r'D:\NSCF Data WG\Current projects\Herbarum mass imaging\Data\Moss'
+
+headers = [
+  'barcode', 
+  'note', 
+  'dwc:family', 
+  'dwc:qualifier', 
+  'dwc:scientificName', 
+  'source', 
+  'identifier', 
+  'dwc:taxonomicStatus', 
+  'acceptedName'
+]
+
+### SCRIPT ###
+
+newfilename = "_all.csv"
+if os.path.exists(os.path.join(dir, newfilename)):
+  os.remove(os.path.join(dir, newfilename))
+
+files = os.listdir(dir)
+csvs = list(filter(lambda x: x.lower().endswith('.csv'), files))
+
+try:
+  with open(os.path.join(dir, newfilename), 'w', encoding='UTF8', newline='') as outfile:
+    writer = csv.DictWriter(outfile, fieldnames= ['filename'] + headers)
+    writer.writeheader()
+    
+    for csvFile in csvs:
+      print('reading', csvFile)
+      with open(os.path.join(dir, csvFile), 'r', encoding="UTF8", errors="ignore") as f:
+        reader = csv.DictReader(f)
+        first = next(reader)
+        csvHeaders = list(first.keys())
+        if headers != csvHeaders:
+          raise Exception('headers in file ' + csvFile, ' don\'t match')
+        
+        first['filename'] = csvFile
+        writer.writerow(first)
+        while True:
+          try:
+            row = next(reader)
+            row['filename'] = csvFile
+            writer.writerow(row)
+          except StopIteration:
+            break
+
+    print('all done...')
+
+except Exception as ex:
+  print(str(ex))
